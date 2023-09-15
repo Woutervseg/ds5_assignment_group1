35da39d6706c7676f40bee65b437c35484643593
chnhongyu <503336@student.fontys.nl>
2023-09-15 09:55:42 +0200

    hoi

--- Week2_ex1.py
+++ Week2_ex1.py
@@ -1,29 +1,49 @@
-file_path = input("Enter the path to the CSV file: ")
-records = []
-with open(file_path, 'r') as file:
-    csv_reader = csv.DictReader(file)
-    for row in csv_reader:
-        records.append(row)
+def read_csv(file_path):
+    """
+    Reads a CSV file and returns a list of records as dictionaries.
+    """
+    records = []
+    with open(file_path, 'r') as file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            records.append(row)
+    return records
 
-def average_func():
+def calculate_average(records):
     """
-    This is a function which calculates the average grade.
+    Calculates the average grade from a list of records.
     """
-    total = sum(float(record['Grade']) for record in a)
-    average = total / len(a)
-    return print(f"Average Grade: {average} \n --------------------")
+    if not records:
+        return None  # Return None if there are no records
 
-filtered_records = [record for record in records if float(record['Grade']) >= 80.0]
+    total = sum(float(record['Grade']) for record in records)
+    average = total / len(records)
+    return average
 
-print("Student Report")
-print("--------------")
-def student_report():
+def print_student_report(records):
     """
-    
+    Prints a student report for records with grades >= 80.
     """
-    for record in filtered_records:
+    print("Student Report")
+    print("--------------")
+    for record in records:
         print(f"Name: {record['Name']}")
         print(f"Grade: {record['Grade']}")
         print("--------------------")
-        return
 
+# Get the CSV file path from the user
+file_path = input("Enter the path to the CSV file: ")
+
+# Read the CSV file and store records
+records = read_csv(file_path)
+
+# Calculate the average grade
+average = calculate_average(records)
+
+# Print the average grade if it's available
+if average is not None:
+    print(f"Average Grade: {average}\n")
+
+# Filter and print the student report for grades >= 80
+filtered_records = [record for record in records if float(record['Grade']) >= 80.0]
+print_student_report(filtered_records)
