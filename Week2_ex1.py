817322c1f16c8177a50344028a0d0d3fe2b77745
TiesVereijken <508276@fontys.nl>
2023-09-15 09:30:07 +0200

    average

--- Week2_ex1.py
+++ Week2_ex1.py
@@ -5,12 +5,10 @@ with open(file_path, 'r') as file:
     for row in csv_reader:
         records.append(row)
 
-def total(record['Grade']):
+def average(record['Grade']):
     total = sum(float(record['Grade']) for record in records)
-    return total
-
-total = sum(float(record['Grade']) for record in records)
-average = total / len(records)
+    average = total / len(records)
+    return average
 
 print(f"Average Grade: {average}")
 print("--------------------")
